fb78290beec139ab63b4010332b2b9be1edc8442
Md. Yeasin Hossain <80535880+yeasin-md@users.noreply.github.com>
2023-11-19 04:18:01 +0600

    Add files via upload

---
+++ src/project/AddInterface.java
@@ -0,0 +1,16 @@
+
+package project;
+
+
+public interface AddInterface {
+    
+    public void addBtn();
+    public void clearBtn();
+    public void editBtn();
+    public void deleteBtn();
+    public void allprdBtn();
+    public void saveBtn();
+    public void clearTableBtn();
+    
+    
+}
---
+++ src/project/loginInterface.java
@@ -0,0 +1,11 @@
+package project;
+public interface loginInterface {
+       
+       public void addData(String user,String pswd);
+       
+       public void checkData(String user,String pswd);
+       
+       public void countLines();
+       
+       public boolean logic(String user,String pswd);
+}
